f5a03550c7ea340788ae864d2e7b7dd3997790f9
Christopher Wright <cmwright33@gmail.com>
2013-08-29 11:34:27 -0400

    edits

--- spec/quizSpec.js
+++ spec/quizSpec.js
@@ -17,3 +17,28 @@ describe('a quiz', function() {
   });
 
 });
+
+describe('the bigDiff function', function() {
+
+  it('it returns the difference', function(){
+    expect(bigDiff([3, 6, 9, 12])).toBe(3);
+
+  });
+
+  it('it returns what i want it to', function(){
+    expect(bigDiff([9, 20, 11, 12])).toBe(8);
+  });
+
+});
+
+
+describe('the averageMedian function', function() {
+
+  it('it returns the averageMedian for a even array length', function() {
+    expect(averageMedian([2, 6, 9, 11])).toBe(7.5);
+  });
+
+  it('it returns the averageMedian for an odd array length', function() {
+    expect(averageMedian([9, 8, 2, 4, 7])).toBe(4.333333333333333);
+  });
+});
--- src/quiz.js
+++ src/quiz.js
@@ -12,6 +12,13 @@ var QUIZ_TOPICS = ['javascript', 'jasmine', 'testing'];
 // bigDiff(array)
 //  [3, 6, 9, 12] => 3
 //  [9, 20, 11, 12] => 8
+function bigDiff(array) {
+  var largest = Math.max.apply( Math, array);
+  var index = array.indexOf(largest);
+  array.splice(index, 1);
+  var second_largest = Math.max.apply(Math, array);
+  return largest - second_largest;
+}
 
 // return the average of the 2 or 3 numbers in the middle of the array
 // (2 if the array has an even number of elements)
@@ -20,12 +27,34 @@ var QUIZ_TOPICS = ['javascript', 'jasmine', 'testing'];
 //  [2, 6, 9, 11] => 7.5
 //  [9, 8, 2, 4, 7] => 4.67
 
+function averageMedian(array) {
+  var array_length = array.length;
+  if (array_length % 2 === 0) {
+    var even_length = (array_length / 2);
+    return (array[even_length - 1] + array[even_length])/2;
+  } else {
+    var odd_length = (array_length / 2) + 0.5;
+    return (array[odd_length - 1] + array[odd_length] + array[odd_length + 1]) / 3;
+  }
+}
+
+
 // return true if you can split the array into parts
 // where the sum of one portion equals the sum of the other portion.
 // canBalance(array)
 //  [1, 2, 1, 1, 1] => true
 //  [2, 1, 1, 2, 1] => false
 //  [3, 4, 1, 2, 3, 1] => true
+
+function canBalance(array) {
+  var subset_array = [];
+  var subset=0;
+  for(var i in array) { total += array[i]; }
+    if (i === array.length) {
+       return false;
+    }
+
+};
 
 // Returns the number of times 2 or more of the same number appear sequentially.
 // countClumps(array)
